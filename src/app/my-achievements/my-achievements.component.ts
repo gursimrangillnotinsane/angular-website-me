d371f31b4ea06a8ed04bf6e6a7123ff2b146293d
Gursimran <gillsimranjot91@gmail.com>
2024-01-13 12:06:46 -0500

    first commit

---
+++ src/app/my-achievements/my-achievements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-achievements',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-achievements.component.html',
+  styleUrl: './my-achievements.component.css'
+})
+export class MyAchievementsComponent {
+
+}
